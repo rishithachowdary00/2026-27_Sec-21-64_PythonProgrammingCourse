8103e6c78c12b25b56707ee0b0c90412c7c7a56c
rishithachowdary00 <rishiyalamanchili22@gmail.com>
2026-09-07 21:40:04 +0530

    Unit-1_Task-13_Membership_Operators

---
+++ Unit-1_Task-13_Membership_Operators.py.py
@@ -0,0 +1,12 @@
+x= 'a' in 'apple'
+print("  result of a in apple is : ",x)
+
+y= 'c' in ' rishi'
+print( " result of c in rishi  is ",y)
+
+
+z= 'v' not in 'pranathi'
+print(" result of v not in pranathi is",z)
+
+a= 'd' not in 'dict'
+print("result of d not in dict is",a )
