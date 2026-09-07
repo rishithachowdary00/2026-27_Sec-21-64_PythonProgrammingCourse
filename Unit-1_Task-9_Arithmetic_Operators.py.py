7a11baf30fdf8db9e47a4b860bbee22d7ad11a46
rishithachowdary00 <rishiyalamanchili22@gmail.com>
2026-09-07 21:25:42 +0530

    Unit-1_Task-9_Arithmetic_Operators

---
+++ Unit-1_Task-9_Arithmetic_Operators.py.py
@@ -0,0 +1,10 @@
+#arithmetic operators
+a=7
+b=2
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("modulus:",a%b)
+print("floor division:",a//b)
+print("exponent:",a**b)
